35f8063dfad04483fde21693b6edc7d297fe6402
Abidul <abd_rmdn@yahoo.com>
2015-01-15 00:15:48 +0800

    template page for individuals for later use

---
+++ individual/indiv_page_template_empty.php
@@ -0,0 +1,23 @@
+	<!--header-->
+	<?php include 'header.php';?>
+	
+	<script>
+		$(document).ready(function() {
+			
+		});
+	</script>
+	<!--Profile-->
+	<section class="wrap mainBody-container">	
+		<div class="row">
+			
+			
+		</div>
+	</section>	
+	<!--Footer-->
+	<?php include '../include/footer.php';?>
+	
+	<!--Popup-->
+	<?php include '../include/popup.php';?>
+
+</body>
+</html>
