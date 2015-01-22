c647d947c68240b96a302a152814f3b7efe24bc6
Abidul <abd_rmdn@yahoo.com>
2015-01-22 14:55:19 +0800

    fixed some of the brand settings style

--- brands/settings.php
+++ brands/settings.php
@@ -1,89 +1,11 @@
-<!DOCTYPE html>
-<!--[if IE 7]>     <html class="ie7" lang="en-us"> <![endif]-->
-<!--[if IE 8]>     <html class="ie8" lang="en-us"> <![endif]-->
-<!--[if IE 9]>     <html class="ie9" lang="en-us"> <![endif]-->
-<!--[if !IE]><!--> <html lang="en-us"> <!--<![endif]-->
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="author" content="Pi">
-	<title>eligo.me</title>
-	<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico">
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
-	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
-	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="../css/style.css?v=1.0" media="all" />
-	<!--[if IE]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-</head>
+<!--Header-->
+	<?php include 'header.php';?>
 
-<body class="inside-page">
-
-	<!--Red top border-->
-	<div class="top-border bg-color--red"></div>
-	
-	
-	
-	<!--Header-->
-	<div class="bg-color--black">	
-	
-		<header>
-			<div class="wrap">
-				<div class="header-container row">
-					<a href="./"><img src="../img/logo.png" alt="eligo logo float-left"></a>
-					<div class="float-right">
-						<img src="../img/credit.png" class="inline img-credit">
-						<div class="inline text-color--white">100</div>
-						<img src="http://www.100open.com/wp-content/uploads/2011/04/Lego-logo.jpeg" class="circle user-avatar--header">
-						<a href="#" class="btn__header__item"><i class="fa fa-bars v-middle text-color--white"></i></a>
-					</div>
-				</div>
-			</div>
-		</header>
-	
-	</div>
-	
-	
-	
-	
-	<!--Cover Photo-->
-	<section class="rel wrap profile__cover">
-	
-		<img src="../img/individual/sample_cover_photo.jpg" class="abs top profile__cover-photo">
-		
-		<div class="rel profile__cover-score">
-			
-			<img src="http://www.100open.com/wp-content/uploads/2011/04/Lego-logo.jpeg" class="inline circle profile__cover-avatar">
-			
-		</div>
-		
-		<div class="abs bottom margin-center text-center profile__cover-userInfo">
-			<div class="profile__cover-copy">
-				<span class="bold h5">Little Elephant</span>
-				<ul class="no-style no-padding no-margin">
-					<li class="profile__cover-userInfo-details">Selangor, Malaysia</li>
-					<li class="profile__cover-userInfo-details">madebysource.com</li>
-					<li class="profile__cover-userInfo-details">Joined December 2014</li>
-				</ul>
-			</div>
-		</div>
-		
-	</section>
-	
-	
-	
-	
-	<!--Individu Navigation-->
-	<section class="wrap text-center">
-		<ul class="bg-color--white no-style no-margin no-padding">
-			<li class="inline"><a href="#" class="user-nav__item bold">Profile</a></li>
-			<li class="inline"><a href="#" class="user-nav__item bold">Create Campaigns</a></li>
-			<li class="inline"><a href="#" class="user-nav__item user-nav__item--active bold">Users Insigths</a></li>
-			<li class="inline"><a href="#" class="user-nav__item bold">Notifications</a></li>
-			<li class="inline"><a href="#"><i class="fa fa-gear user-nav__fa-gear"></i></a></li>
-		</ul>
-	</section>
-	
-	
+	<script>
+        $(document).ready(function(event){
+            $('.toggle')
+        });
+</script>
 	
 	
 	<!--Profile-->
@@ -97,7 +19,7 @@
 			<div class="row">
 				<table class="table-brand__settings" width="100%">
 					<tr>
-						<th colspan="3">Signing in</th>
+						<th colspan="3" class="light">Signing in</th>
 					</tr>
 					<tr>
 						<td class="col3">							
@@ -106,13 +28,13 @@
 									<th colspan="3">Basic</th>
 								</tr>
 								<tr>
-									<td rowspan="2"><a href="#" class="brand__setting-img"><img src="http://www.100open.com/wp-content/uploads/2011/04/Lego-logo.jpeg"></a></td>
+									<td rowspan="2" ><a href="#" class=""><img src="http://www.100open.com/wp-content/uploads/2011/04/Lego-logo.jpeg" class="brand__setting-img"></a></td>
 									<td style="text-align: middle;"><a href="#" class="brand__setting-icon brand__setting-icon--facebook"><i class="fa fa-facebook"></i></a></td>
-									<td>/teehow <span>disconenct from facebook<span></td>
+									<td>/teehow </td>
 								</tr>
 								<tr>
-									<td style="text-align: middle;"><a href="#" class="brand__setting-icon brand__setting-icon--google-plus"><i class="fa fa-google-plus"></i></a></td>
-									<td>/teehow <span>disconenct from google+<span></td>
+									<td style="text-align: left;"><a href="#" class=" left brand__setting-icon brand__setting-icon--google-plus"><i class="fa fa-google-plus"></i></a></td>
+									<td class="left">/teehow </td>
 								</tr>
 							</table>
 						</td>
@@ -124,10 +46,6 @@
 								<tr>
 									<td class="brandSettings__form--label">Email</td>
 									<td>teehow@gmail.com</td>
-								</tr>
-								<tr>
-									<td class="brandSettings__form--label">Password</td>
-									<td>Last changed: 5 Dec 2014</td>
 								</tr>
 							</table>
 						</td>
@@ -146,48 +64,62 @@
 				</table>
 				<table class="table-brand__settings" width="100%">
 					<tr>
-						<th colspan="3">Notification</th>
+						<th colspan="3" class="light">Email Notification</th>
 					</tr>
 					<tr>
 						<td class="col3">
 							<span  class="bold h6">Web</span>
 							<span>
-								<select class="brandSettings__form brandSettings__form--select">
-									<option>As they happen</option>
-								</select>
-							</span>
-							
-							<span>
-								<label class="brandSettings__form brandSettings__form--checkboxLabel"><input type="checkbox" /> Users applied your campaign</label>
-								<label class="brandSettings__form brandSettings__form--checkboxLabel"><input type="checkbox" /> Users accepted/declined invitation</label>
-								<label class="brandSettings__form brandSettings__form--checkboxLabel"><input type="checkbox" /> Campaign reminder</label>
-								<label class="brandSettings__form brandSettings__form--checkboxLabel"><input type="checkbox" /> Transaction status</label>
+								<label class="brandSettings__form brandSettings__form--checkboxLabel"><input type="checkbox" class="custom-checkbox-field" name="-" checked/> Users applied your campaign</label>
+								<label class="brandSettings__form brandSettings__form--checkboxLabel"><input type="checkbox" class="custom-checkbox-field" name="-" /> Users accepted/declined invitation</label>
+                                <span>
+                                    <div class="brandSettings__form ">
+                                        <input type="checkbox" class="circular-checkbox" name="email_timing[]">once a day &nbsp;&nbsp;
+                                        <input type="checkbox" class="circular-checkbox" name="email_timing[]">As they happen
+                                    </div>
+							    </span>
+								<label class="brandSettings__form brandSettings__form--checkboxLabel"><input type="checkbox" class="custom-checkbox-field" name="-" /> Campaign reminder</label>
 							</span>
 						</td>
 						<td class="col3">
-							<span  class="bold h6">by email</span>
-							<span>
-								<select class="brandSettings__form brandSettings__form--select">
-									<option>As they happen</option>
-								</select>
-							</span>
+							<span  class="bold h6">We'll also let you know about: </span>
+							
 							
 							<span>
-								<label class="brandSettings__form brandSettings__form--checkboxLabel"><input type="checkbox" /> Users applied your campaign</label>
-								<label class="brandSettings__form brandSettings__form--checkboxLabel"><input type="checkbox" /> Users accepted/declined invitation</label>
-								<label class="brandSettings__form brandSettings__form--checkboxLabel"><input type="checkbox" /> Campaign reminder</label>
-								<label class="brandSettings__form brandSettings__form--checkboxLabel"><input type="checkbox" /> Transaction status</label>
+                                <label class="brandSettings__form brandSettings__form--checkboxLabel"><input type="checkbox" class="custom-checkbox-field" name="-" checked /> New Message</label>
+                                <span>
+                                    <div class="brandSettings__form ">
+                                        <input type="checkbox" class="circular-checkbox" name="web_timing[]">once a day &nbsp;&nbsp;
+                                        <input type="checkbox" class="circular-checkbox" name="web_timing[]">As they happen
+                                    </div>
+							    </span>
+                                <label class="brandSettings__form brandSettings__form--checkboxLabel ">
+                                    <input type="checkbox" class="custom-checkbox-field" name="-" /> Transaction status 
+                                </label>
+								<label class="brandSettings__form brandSettings__form--checkboxLabel"><input type="checkbox" class="custom-checkbox-field" name="-" checked /> Announcements about new features</label>
+								<label class="brandSettings__form brandSettings__form--checkboxLabel"><input type="checkbox" class="custom-checkbox-field" name="-" /> Invitation to give us feedback</label>
+								
+								
 							</span>
 						</td>
 						<td class="col3 bold h6"></td>
 					</tr>
 				</table>
+                <form method="POST" action="this">
+                    <div class="float-right"><input type="submit" class="btn btn__bg-color--red" value="DELETE ACCOUNT"/></div>
+                </form>
 			</div>
 		</div>
 	</section>
 	
 	
-	
+<!--
+	<div class=" float-left inline custom-checkbox"><div class="decoration actived disabled">
+        <div class="lbl-text light inline float-left">Yes</div>
+        <div class="toggle inline bg-color--white"><a href=""></a></div>
+        <div class="lbl-text light inline float-right">No</div>
+    </div><input type="checkbox" name="chkbx_empty" ></div>
+-->
 	
 	<!--Footer-->
 	<?php include '../include/footer.php';?>
