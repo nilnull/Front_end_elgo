55772d34943960c8607cd7267bd4aacb43345c5a
Araz Farhang <arazfarhang@MacBook-Air.local>
2015-02-05 14:31:35 +0800

    generated campaign prev with map

---
+++ brands/Campain_Stage2_Priview.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<link href="../css/style.css" rel="stylesheet" type="text/css">
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+
+<body>
+<div class="container">
+<header></header>
+<div class="popup-content"> 
+  <!-- Campain and brand info -->
+  <div class="Campaign-header" id "box" > 
+    <!-- Close Button --> 
+    
+    <a class="boxclose" id="boxclose"></a> 
+    
+    <!-- brand Name -->
+    
+    <h1 id="company-name" class="text-color--grey--darker title bold
+ brand-profile Campaign-card-camp-name">Company Name</h1>
+  </div>
+  
+  <!-- Campaign image -->
+  
+  <div id="campaign_prev"> <img id="campaign-img" style="display:block" src="img/individual/sample_camp" widthalt=""/> </div>
+  
+  <!-- Campaign info -->
+  
+  <div id="campaign-content">
+  
+  
+    <!-- Campain name -->
+ <div id="capaign-header">
+ <div class="campaign-title inline ">
+    <div class="brands-circular-logo circle" style="background-image:url(img/individual/petronas_logo.jpg)"></div>
+       
+    <h2 class="text-color--grey--darker  title Campaign-card-brand-name text-size-medium light"> Petronas Brands </h2>
+    
+    </div>
+    <div  class="campaign-participate float-right">
+    <div class=" bg-color--red text-color--white right float-right medium-padding text-center text-size-small left-large-pad right-large-pad">
+    Apply
+    </div>
+    <div class="right text-size-xlarge medium-padding  float-right">
+    Check In
+    </div>
+    
+    </div>
+    </div>
+    <!-- Campain detail -->
+    
+    <h3 class="text-color--grey--darker title">Brief</h3>
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec nulla pretium, faucibus ipsum id, dictum enim. Donec nulla nisl, pretium id pharetra eu, lobortis sodales neque. Etiam commodo venenatis vehicula. Nunc luctus varius nisi, sed bibendum magna ultricies vel. Nunc maximus risus augue, vitae mollis lacus lobortis quis. Donec a dolor sit amet justo aliquet vehicula ac vel urna. Praesent eget ultricies turpis. Fusce in efficitur ipsum, nec elementum nulla. Duis ac tempus diam, vel ornare ex. Vivamus consectetur sodales faucibus. Sed tempor blandit nulla, a pretium nisl varius a. Ut tincidunt leo quis lectus fermentum, vel iaculis odio ullamcorper. Praesent vel cursus sapien, vel rhoncus nisl. Mauris sed sodales augue, sit amet gravida erat. </p>
+    <h3 class="text-color--grey--darker title">Campaign Period</h3>
+    <p> 12/13/2015 - 19/23/2017 </p>
+    <h3 class="text-color--grey--darker title">Check In</h3>
+    <p> <a href="#js-step2--checkin" class="circle btn__bg-color--pink  js-popup"> </a> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec nulla pretium, faucibus ipsum id, dictum enim. Donec nulla nisl, pretium id pharetra eu, lobortis sodales neque. Etiam commodo venenatis vehicula. Nunc luctus varius nisi, sed bibendum magna ultricies vel. Nunc maximus risus augue, vitae mollis lacus lobortis. <a class="text-color--red">click here</a> </p>
+    <p>
+    <div class="campaign-detail row">
+      <div class="campaign-map"></div>
+      <div class="campaign-map-description">
+        <h3 class="text-color--grey--darker title">Pvillion Kuala Lampur</h3>
+        <p> A shoping mall in kl </p>
+        <h3 class="text-color--grey--darker text-size-large title">How to:</h3>
+        <p>
+        <ul class="numbered-list">
+          <li> .Items</li>
+          <?php $i=0; while($i<5){?>
+          <li>aware that the CSS for these layouts is</li>
+          <?php $i++; }; ?>
+        </ul>
+        </p>
+        
+        <!-- Campain price -->
+        
+        <h3 class="text-color--grey--darker text-size-large title topgap-large">PAY* Per Check-In : 0.03</h3>
+      </div>
+    </div>
+    </p>
+    <div class="hr bg-color--grey topgap-small">
+      </di>
+      <div class="right text-color--grey--lighter
+ text-right text-size-medium topgap-small"> * Payments accours once per check-in </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
